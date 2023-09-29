bf2217f75533c0c331b13890eca7a8ca0f280c6b
rubenixter <124615627+rubenixter@users.noreply.github.com>
2023-09-29 19:59:55 +0200

    Some test smells fixing

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -1,53 +1,66 @@
 package es.upm.grise.profundizacion;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Vector;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-	Calculator c;
-	@BeforeEach
-	void init() {
-		 c=new Calculator();	
-	}
-	
-	@Test
-	void testAdd() {
-		c.add(1);
-		assertEquals(1,c.min());
-	}
-	@Test
-	void testRemove() {
-		c.add(1);
-		c.add(3);
-		c.remove(1);
-		assertEquals(3,c.min());
-	}
-	@Test
-	void testMax() {
-		c.add(1);
-		c.add(3);
-		assertEquals(3,c.max());
-	}
-	@Test
-	void testMin() {
-		c.add(1);
-		c.add(3);
-		assertEquals(1,c.min());
-	}
-	@Test
-	void testAvg() {
-		c.add(1);
-		c.add(3);
-		assertEquals(2,c.average());
-	}
-	@Test
+    Calculator c;
+    static final double VALUE_ONE = 1.0;
+    static final double VALUE_TWO = 2.0;
+    static final double VALUE_THREE = 3.0;
+
+    @BeforeEach
+    void setUp() {
+        c = new Calculator();
+    }
+
+    @Test
+    void testAdd() {
+        c.add(VALUE_ONE);
+        double res=c.min();
+        assertEquals(VALUE_ONE,res );
+    }
+
+    @Test
+    void testRemove() {
+        c.add(VALUE_ONE);
+        c.add(VALUE_THREE);
+        c.remove(VALUE_ONE);
+        double res=c.min();
+        assertEquals(VALUE_THREE, res);
+    }
+
+    @Test
+    void testMax() {
+        c.add(VALUE_ONE);
+        c.add(VALUE_THREE);
+        double res=c.max();
+        assertEquals(VALUE_THREE,res);
+    }
+
+    @Test
+    void testMin() {
+        c.add(VALUE_ONE);
+        c.add(VALUE_THREE);
+        double res=c.min();
+        assertEquals(VALUE_ONE, res);
+    }
+
+    @Test
+    void testAvg() {
+        c.add(VALUE_ONE);
+        c.add(VALUE_THREE);
+        double res=c.average();
+        assertEquals(VALUE_TWO,res );
+    }
+
+    @Test
 	void testStdDev() {
-		c.add(1);
-		c.add(2);
-		assertEquals(0.5,c.stddev());
+		c.add(VALUE_ONE);
+		c.add(VALUE_TWO);
+		double res=c.stddev();
+		double sol=0.5;
+		assertEquals(sol,res);
 	}
 }
