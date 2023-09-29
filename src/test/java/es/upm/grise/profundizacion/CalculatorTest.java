41765e8a48df59553b84002b662d4e627bfabed6
rubenixter <124615627+rubenixter@users.noreply.github.com>
2023-09-29 19:13:32 +0200

    New tests

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -4,13 +4,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Vector;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-
-	@Test
-	void test() {
-		fail();
+	Calculator c;
+	@BeforeEach
+	void init() {
+		 c=new Calculator();	
 	}
-
+	
+	@Test
+	void testAdd() {
+		c.add(1);
+		assertEquals(1,c.min());
+	}
+	@Test
+	void testRemove() {
+		c.add(1);
+		c.add(3);
+		c.remove(1);
+		assertEquals(3,c.min());
+	}
+	@Test
+	void testMax() {
+		c.add(1);
+		c.add(3);
+		assertEquals(3,c.max());
+	}
+	@Test
+	void testMin() {
+		c.add(1);
+		c.add(3);
+		assertEquals(1,c.min());
+	}
+	@Test
+	void testAvg() {
+		c.add(1);
+		c.add(3);
+		assertEquals(2,c.average());
+	}
+	@Test
+	void testStdDev() {
+		c.add(1);
+		c.add(2);
+		assertEquals(0.5,c.stddev());
+	}
 }
