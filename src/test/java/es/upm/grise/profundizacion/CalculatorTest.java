b58a25713b05d678a2c89bbf6c5e4ccdb54a49f9
aloncrack7 <61496485+aloncrack7@users.noreply.github.com>
2023-09-30 19:51:46 +0200

    Add: test and fixes

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -4,13 +4,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Vector;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	static Calculator calculadora;
 
-	@Test
-	void test() {
-		fail();
+	@BeforeAll
+	static void prepareCalculator(){
+		calculadora = new Calculator();
 	}
 
+	@BeforeEach
+	void prepareData(){
+		calculadora.add(2);
+		calculadora.add(3);
+	}
+
+	@AfterEach
+	void removeData(){
+		calculadora.remove(2);
+		calculadora.remove(3);
+	}
+
+	@Test
+	void testMax(){
+		assertEquals(3, calculadora.max(), "El maximo entre 2 y 3 es 3");
+	}
+
+	@Test
+	void testMin(){
+		assertEquals(2, calculadora.min(), "El minimo entre 2 y 3 es 3");
+	}
+
+	@Test
+	void testAvg(){
+		assertEquals(2.5, calculadora.average(), "La media entre 2 y 3 es 2.5");
+	}
+
+	@Test
+	void testStdDev(){
+		assertEquals(0.5, calculadora.stddev(), "La desviacion tipica entre 2 y 3 es 0.5");
+	}
 }
