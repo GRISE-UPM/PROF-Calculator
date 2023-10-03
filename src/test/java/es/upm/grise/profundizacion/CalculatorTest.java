4bca3ce7a2d34da8cc3f63212b2a4f62cb2d52de
AlainSQ <alainsanchez.ep@gmail.com>
2023-10-03 19:06:13 +0200

    add test

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -1,16 +1,65 @@
 package es.upm.grise.profundizacion;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.util.Vector;
 
-import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
 
-	@Test
-	void test() {
-		fail();
-	}
+public class CalculatorTest {
+    private Calculator calculator;
+    
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        calculator.numbers=new Vector <Double> ();
+    }
 
+    @Test
+    public void testAdd() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        assertEquals(2, calculator.numbers.size());
+    }
+
+    @Test
+    public void testRemove() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        calculator.remove(10.0);
+        assertEquals(1, calculator.numbers.size());
+    }
+
+    @Test
+    public void testMax() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        calculator.add(5.0);
+        assertEquals(20.0, calculator.max(), 0.001);
+    }
+
+    @Test
+    public void testMin() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        calculator.add(5.0);
+        assertEquals(5.0, calculator.min(), 0.001);
+    }
+
+    @Test
+    public void testAverage() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        calculator.add(30.0);
+        assertEquals(20.0, calculator.average(), 0.001);
+    }
+
+    @Test
+    public void testStddev() {
+        calculator.add(10.0);
+        calculator.add(20.0);
+        calculator.add(30.0);
+        assertEquals(100.0, calculator.stddev(), 0.001);
+    }
 }
