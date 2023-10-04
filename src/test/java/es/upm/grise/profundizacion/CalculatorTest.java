f193a03540efa24d012b248631e339f27e5f7624
AlainSQ <alainsanchez.ep@gmail.com>
2023-10-04 16:59:04 +0200

    test without smells

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Vector;
 
-
-
 public class CalculatorTest {
     private Calculator calculator;
     
@@ -14,52 +12,49 @@ public class CalculatorTest {
     public void setUp() {
         calculator = new Calculator();
         calculator.numbers=new Vector <Double> ();
+        double valor1=10.0;
+        double valor2=20.0;
+        double valor=20.0;
+        calculator.add(valor1);
+        calculator.add(valor2);
+   
     }
 
     @Test
     public void testAdd() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        assertEquals(2, calculator.numbers.size());
+        int esperado=3;
+        calculator.add(valor);
+        assertEquals("Añade un valor", esperado, calculator.numbers.size());
     }
 
     @Test
     public void testRemove() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        calculator.remove(10.0);
-        assertEquals(1, calculator.numbers.size());
+        int esperado=1;
+        calculator.remove(valor);
+        assertEquals("Elimina un valor", esperado, calculator.numbers.size());
     }
 
     @Test
     public void testMax() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        calculator.add(5.0);
-        assertEquals(20.0, calculator.max(), 0.001);
+        double esperado=20.0;
+        assertEquals("max 20", esperado, calculator.max());
     }
 
     @Test
     public void testMin() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        calculator.add(5.0);
-        assertEquals(5.0, calculator.min(), 0.001);
+        double esperado=10.0;
+        assertEquals("min 10", esperado, calculator.min());
     }
 
     @Test
     public void testAverage() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        calculator.add(30.0);
-        assertEquals(20.0, calculator.average(), 0.001);
+        double esperado=15.0;
+        assertEquals("Media 15", esperado, calculator.average());
     }
 
     @Test
     public void testStddev() {
-        calculator.add(10.0);
-        calculator.add(20.0);
-        calculator.add(30.0);
-        assertEquals(100.0, calculator.stddev(), 0.001);
+        double esperado=100.0;
+        assertEquals("Std 100",esperado, calculator.stddev());
     }
 }
