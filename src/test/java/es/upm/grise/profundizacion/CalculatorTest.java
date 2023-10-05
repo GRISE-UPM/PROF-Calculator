ba1bccacb35b4fac71c2c4f6f3c60194411572e5
LuisBMI <luisbmi55@gmail.com>
2023-10-05 13:37:35 +0200

    Tests added

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -4,13 +4,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Vector;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	
+	Calculator calculator;
+	final int lowest = 1, middle = 2, highest = 3;  // elements of the vector
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		calculator = new Calculator();
+		calculator.numbers = new Vector<Double>();
+		calculator.add(lowest);
+		calculator.add(middle);
+		calculator.add(highest);
+	}
 
 	@Test
-	void test() {
-		fail();
+	public void testAdd() {
+		final int aditional = 4;
+		calculator.add(aditional);
+		assertEquals(aditional, calculator.numbers.get(calculator.numbers.size() - 1));
+	}
+
+	@Test
+	public void testRemove() {
+		calculator.remove(middle);
+		assertEquals(middle, calculator.numbers.size());
+	}
+
+	@Test
+	public void testMax() {
+		assertEquals(highest, calculator.max());
+	}
+
+	@Test
+	public void testMin() {
+		assertEquals(lowest, calculator.min());
+	}
+
+	@Test
+	public void testAverage() {
+		assertEquals(middle, calculator.average());
+	}
+
+	@Test
+	public void testStddev() {
+		assertEquals(lowest, calculator.stddev());
 	}
 
 }
