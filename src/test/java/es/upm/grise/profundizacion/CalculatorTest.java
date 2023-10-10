94dbc7f26a5e60b7764c95f2ef98ee038c08de06
antoniocastajr <147398639+antoniocastajr@users.noreply.github.com>
2023-10-10 15:58:41 +0200

    Update CalculatorTest.java

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -8,9 +8,37 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-	@Test
-	void test() {
-		fail();
-	}
+    @Test
+    public void testMax() {
+        Calculator calculator = new Calculator();
+        calculator.add(10.0);
+        calculator.add(20.0);
+        assertEquals(20.0, calculator.max(), 0.001);
+    }
+
+    @Test
+    public void testMin() {
+        Calculator calculator = new Calculator();
+        calculator.add(10.0);
+        calculator.add(20.0);
+        assertEquals(10.0, calculator.min(), 0.001);
+    }
+
+    @Test
+    public void testAverage() {
+        Calculator calculator = new Calculator();
+        calculator.add(10.0);
+        calculator.add(20.0);
+        assertEquals(15.0, calculator.average(), 0.001);
+    }
+
+    @Test
+    public void testStdDev() {
+        Calculator calculator = new Calculator();
+        calculator.add(10.0);
+        calculator.add(20.0);
+        assertEquals(5.0, calculator.stddev(), 0.001);
+    }
 
 }
+
