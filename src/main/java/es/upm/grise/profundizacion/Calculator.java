ed052e32476cc591a964386d721bdadd489f7d99
antoniocastajr <147398639+antoniocastajr@users.noreply.github.com>
2023-10-10 15:57:52 +0200

    Update Calculator.java

--- src/main/java/es/upm/grise/profundizacion/Calculator.java
+++ src/main/java/es/upm/grise/profundizacion/Calculator.java
@@ -5,7 +5,7 @@ import java.lang.Double;
 
 public class Calculator {
 
-	public Vector <Double> numbers;
+	public Vector <Double> numbers = new Vector()<Double>; // Inicialización del vector
 	
 	// Public methods
 	//
