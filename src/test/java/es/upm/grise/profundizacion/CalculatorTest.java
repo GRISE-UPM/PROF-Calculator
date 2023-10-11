b51b267e1eaaeb3fb993a092390ab7ea35e174cc
Anibal Rivero <anibal.rivero.rios@alumnos.upm.es>
2023-10-11 14:32:36 +0200

    Fixed Assertion Roulette tests

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -9,65 +9,67 @@ class CalculatorTest {
 
 
 	private Calculator calc;
-	@BeforeEach
-	void calcInit() {
-		calc = new Calculator();
-		calc.numbers = new Vector<>();
-		calc.add(1);
-		calc.add(-23);
-		calc.add(23);
-		calc.add(16);
-		calc.add(23);
-		calc.add(21);
-		calc.add(16);
-	}
+	@Nested
+	class BasicOperation_Tests {
+		@BeforeEach
+		void calcInit() {
+			calc = new Calculator();
+			calc.numbers = new Vector<>();
+			calc.add(1);
+			calc.add(-23);
+			calc.add(23);
+			calc.add(16);
+			calc.add(23);
+			calc.add(21);
+			calc.add(16);
+		}
 
-	@Test
-	@DisplayName("Checks max function when some numbers are negative")
-	void maxNegative() {
-		final int MAX = 23;
-		assertEquals(MAX,calc.max(),"Calculated incorrect max when using negative numbers");
-	}
+		@Test
+		@DisplayName("Checks max function when some numbers are negative")
+		void maxNegative() {
+			final int MAX = 23;
+			assertEquals(MAX,calc.max(),"This test fails if incorrect max was calculated when using negative numbers");
+		}
 
-	@Test
-	@DisplayName("Checks min function when some numbers are negative")
-	void minNegative() {
-		final int MIN = -23;
+		@Test
+		@DisplayName("Checks min function when some numbers are negative")
+		void minNegative() {
+			final int MIN = -23;
 
-		assertEquals(MIN,calc.min(), "Calculated incorrect min when using negative numbers");
-	}
+			assertEquals(MIN,calc.min(), "This test fails if incorrect min was calculated when using negative numbers");
+		}
 
-	@Test
-	@DisplayName("Checks Average function when some numbers are negative")
-	void averageNegative() {
-		final int AVERAGE = 11;
-		assertEquals(AVERAGE,calc.average(),"Calculated Average is incorrect when using negatives");
-	}
-	@Test
-	@DisplayName("Checks that stddev gives the correct value even with negative numbers")
-	void standardDeviationNegative() {
-		final double DEVIATION = 282.33333333333;
-		final double DELTA = 0.0001;
-		assertEquals(DEVIATION,calc.stddev(),DELTA, "Calculated deviation is incorrect when using negatives");
-	}
+		@Test
+		@DisplayName("Checks Average function when some numbers are negative")
+		void averageNegative() {
+			final int AVERAGE = 11;
+			assertEquals(AVERAGE,calc.average(),"This test fails when Calculated Average is incorrect when using negatives");
+		}
+		@Test
+		@DisplayName("Checks that stddev gives the correct value even with negative numbers")
+		void standardDeviationNegative() {
+			final double DEVIATION = 282.33333333333;
+			final double DELTA = 0.0001;
+			assertEquals(DEVIATION,calc.stddev(),DELTA, "This test fails when calculated deviation is incorrect when using negatives");
+		}
 
-	@Test
-	@DisplayName("Checks the size of the number's vector")
-	void addSize() {
-		final int SIZE = 7;
-		assertEquals(SIZE,calc.numbers.size(),"TEST");
+		@Test
+		@DisplayName("Checks the size of the number's vector")
+		void addSize() {
+			final int SIZE = 7;
+			assertEquals(SIZE,calc.numbers.size(),"This test fails if the calculator fails to keep track of its size");
+		}
+		@Test
+		@DisplayName("Checks the content of the number's vector")
+		void addContent() {
+			assertArrayEquals(new Double[] {1.0, -23.0, 23.0, 16.0, 23.0, 21.0, 16.0},calc.numbers.toArray(), "This test fails if numbers were added incorrectly to the calculator");
+		}
+		@Test
+		@DisplayName("Checks that the last number is removed")
+		void remove() {
+			calc.remove(-23);
+			assertArrayEquals(new Double[] {1.0, 23.0, 16.0, 23.0, 21.0, 16.0},calc.numbers.toArray(), "This test fails if the calculator was unable to properly remove number");
+		}
 	}
-	@Test
-	@DisplayName("Checks the content of the number's vector")
-	void addContent() {
-		assertArrayEquals(new Double[] {1.0, -23.0, 23.0, 16.0, 23.0, 21.0, 16.0},calc.numbers.toArray(), "Numbers were added incorrectly to the calculator");
-	}
-	@Test
-	@DisplayName("Checks that the last number is removed")
-	void remove() {
-		calc.remove(-23);
-		assertArrayEquals(new Double[] {1.0, 23.0, 16.0, 23.0, 21.0, 16.0},calc.numbers.toArray(), "Unable to properly remove number");
-	}
-
 
 }
